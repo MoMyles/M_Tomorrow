978cb2da1f45ea441ff1a00f3f649c6630358b31
momyles <maogd_0916@163.com>
2018-06-15 13:53:29 +0800

    Add ClickUtil

---
+++ app/src/main/java/cc/momyles/tomorrow/util/ClickUtil.java
@@ -0,0 +1,19 @@
+package cc.momyles.tomorrow.util;
+
+/**
+ * 防止快速重复点击
+ */
+public class ClickUtil {
+    private static final int MIN_DELAY_TIME = 1000;  // 两次点击间隔不能少于1000ms
+    private static long lastClickTime;
+
+    public static boolean isFastClick() {
+        boolean flag = true;
+        long currentClickTime = System.currentTimeMillis();
+        if ((currentClickTime - lastClickTime) >= MIN_DELAY_TIME) {
+            flag = false;
+        }
+        lastClickTime = currentClickTime;
+        return flag;
+    }
+}
