ce7da2f1772eb18fc87708ace634546e8bcd4e14
momyles <maogd_0916@163.com>
2018-08-13 13:31:34 +0800

    some better

--- m/src/main/java/com/cetcme/m/net/RequestHelper.java
+++ m/src/main/java/com/cetcme/m/net/RequestHelper.java
@@ -1,13 +1,9 @@
 package com.cetcme.m.net;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AlertDialog;
-import android.widget.RelativeLayout;
 
-import com.wang.avi.AVLoadingIndicatorView;
-import com.wang.avi.indicators.BallSpinFadeLoaderIndicator;
+import com.cetcme.m.helper.LoadingHelper;
 
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -19,19 +15,7 @@ public class RequestHelper {
     private static AlertDialog alert = null;
 
     public RequestHelper(Context context) {
-        final RelativeLayout rl = new RelativeLayout(context);
-        final AVLoadingIndicatorView loadingIndicatorView = new AVLoadingIndicatorView(context);
-        loadingIndicatorView.setIndicator(new BallSpinFadeLoaderIndicator());
-
-        RelativeLayout.LayoutParams rlp = new RelativeLayout.LayoutParams(128, 128);
-        rlp.addRule(RelativeLayout.CENTER_IN_PARENT);
-        loadingIndicatorView.setLayoutParams(rlp);
-        rl.addView(loadingIndicatorView);
-
-        alert = new AlertDialog.Builder(context)
-                .setView(rl)
-                .create();
-        alert.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        alert = LoadingHelper.defaultLoading(context);
     }
 
     public <T> Disposable doRequestWithLoading(Observable<T> observable, final OnRequestListener<T> requestListener) {
